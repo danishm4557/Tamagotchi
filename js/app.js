431d6534aff55f4b85dbdbb5d1cbff556d06c160
danishm4557 <danishm4557@gmail.com>
2021-12-08 15:01:11 -0600

    Added intervals for game's main componenets

--- js/app.js
+++ js/app.js
@@ -2,15 +2,19 @@
 //   const startButtons = document.querySelector('.button')
 //   startButtons.addEventListner('click', '')
 // }
+// const audioUnmute = document.querySelector('#mw2Video')
+// audioUnmute.muted = false
+
+
 
 class Tamagotchi {
   constructor(name) {
     this.name = name
-    this.hunger = Math.floor(Math.random() * 20)   // Hunger level starts at random. If it goes above 20, Tamagotchi dies.
-    this.sleepiness = Math.floor(Math.random() * (0 - 20)) + 20   // sleepiness level starts at random. If it goes above 20, Tamagotchi dies.
-    this.boredom = Math.floor(Math.random() * (0 - 20)) + 20    // Boredom level starts at random. If it goes above 20, Tamagotchi dies.
+    this.hunger = Math.floor(Math.random() * 15)   // Hunger level starts at random. If it goes above 20, Tamagotchi dies.
+    this.boredom = Math.floor(Math.random() * (0 - 15)) + 15    // Boredom level starts at random. If it goes above 20, Tamagotchi dies.
+    this.sleepiness = Math.floor(Math.random() * (0 - 15)) + 15   // sleepiness level starts at random. If it goes above 20, Tamagotchi dies.
+    this.age = 0
   }
-
 
   feedFunction() {
     console.log("now feeding");
@@ -19,16 +23,11 @@ class Tamagotchi {
     if (this.hunger <= 1) {
       console.log("Oh man I'm so full");
     }
-  }
-
-
-  sleepFunction() {
-    console.log("now sleeping");
-    this.sleepiness -= 2;
-    console.log(this.sleepiness);
-    if (this.sleepiness <= 1) {
-        console.log("I'm wide awake!");
-    }
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    backgroundColor.style.backgroundColor = bgColor
   }
 
 
@@ -39,8 +38,32 @@ class Tamagotchi {
     if (this.boredom <= 1) {
       console.log("I love this, can we do this again some other time?!");
     }
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    backgroundColor.style.backgroundColor = bgColor
+  }
+
+  sleepFunction() {
+    console.log("now sleeping");
+    this.sleepiness -= 2;
+    console.log(this.sleepiness);
+    if (this.sleepiness <= 1) {
+        console.log("I'm wide awake!");
+    }
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    backgroundColor.style.backgroundColor = bgColor
   }
 }
+//   submitNameFunction() {
+//     setInterval(this.timer++, 1000)
+//     setInterval(console.log(this.timer), 1000);
+//   }
+// }
 
 //////////////////////// INSTANTIATING CHARACTER ////////////////////
 const character = new Tamagotchi('babu_G_1');
@@ -48,33 +71,225 @@ console.log(character);
 
 
 
-//////////////// DECREASE HUNGER LEVEL WITH BUTTON /////////////////
-const feedButton = document.querySelector('#left-button')
-feedButton.addEventListener('click', ()=> {
-  if (character.hunger > 1) {
-    character.feedFunction()
-  }
-})
+////////////////////// SETTING UP AGE ////////////////////////
+
+  const ageCount = document.querySelector('#ageP')
+  const ageInterval = setInterval(() => {
+    if (character.age >= 24) {
+      clearInterval(timerInterval)
+    }
+    character.age += 1
+    ageCount.innerText = `Age: ${character. age}`
+  }, 5000)
 
 
 
 
 
-//////////////// DECREASE BOREDOM LEVEL WITH BUTTON /////////////////
-const boredomButton = document.querySelector('#middle-button')
-boredomButton.addEventListener('click', ()=> {
-  if (character.boredom > 1) {
-    character.boredomFunction()
-  }
-})
+  ////////////// SETTING UP START GAME FUNCTION ////////////////
+  const nameSubmit = document.querySelector('#submitButton')
+  const gameOver = document.querySelector('#gameOver')
+  const soldierCharacter = document.querySelector('.soldier')
+  const allButtons = document.querySelectorAll('button')
+  nameSubmit.addEventListener('click', ()=> {
+    nameSubmit.setAttribute('class', 'soldierRotate')
+    let gameTitle = document.querySelector('title')
+    let inputName = document.querySelector('input')
+    gameTitle.innerText = inputName.value + "'s Tamagotchi"
+    inputName.remove()
+    nameSubmit.disable = true
+    nameSubmit.style.opacity = 0
+
+    console.log(character.age);
+  })
 
 
 
 
-//////////////// DECREASE SLEEPINESS LEVEL WITH BUTTON /////////////////
-const sleepButton = document.querySelector('#right-button')
-sleepButton.addEventListener('click', ()=> {
-  if (character.sleepiness > 1){
-    character.sleepFunction()
-  }
-})
+
+
+
+  //////////////// INCREASE HUNGER LEVEL WITH TIME /////////////////
+  const intervalX = setInterval(() => {
+    if (character.hunger <= 18) {
+      character.hunger += 2
+      hungerCount.innerText = `Hunger Level: ${character.hunger}`
+
+    } else {
+      clearInterval(intervalX)
+      clearInterval(intervalY)
+      clearInterval(intervalZ)
+      clearInterval(ageInterval)
+      gameOver.style.opacity = 100
+      soldierCharacter.style.opacity = 0
+    }
+  }, 3000)
+
+
+  //////////////// DECREASE HUNGER LEVEL WITH BUTTON (FEED) /////////////////
+    const backgroundColor = document.querySelector('#tamogotchi-frame')
+    const feedButton = document.querySelector('#left-button')
+    const hungerCount = document.querySelector('#hunger')
+    const feedEnableButton = () => {
+      feedButton.disabled = false
+    }
+
+    feedButton.addEventListener('click', ()=> {
+      feedButton.disabled = true;
+      hungerCount.style.padding = 5 + 'px';
+      character.feedFunction()
+      hungerCount.innerText = `Hunger Level: ${character.hunger}`
+      setTimeout(feedEnableButton, 5000)
+    })
+
+
+
+
+
+
+
+
+
+    //////////////// INCREASE BOREDOM LEVEL WITH TIME /////////////////
+    const intervalY = setInterval(() => {
+      if (character.boredom <= 18) {
+        character.boredom += 2
+        boredomCount.innerText = `Boredom Level: ${character.boredom}`
+
+      } else {
+        clearInterval(intervalY)
+        clearInterval(intervalX)
+        clearInterval(intervalZ)
+        clearInterval(ageInterval)
+        gameOver.style.opacity = 100
+        soldierCharacter.style.opacity = 0
+      }
+    }, 3000)
+
+
+    //////////////// DECREASE BOREDOM LEVEL WITH BUTTON (FEED) /////////////////
+      const boredomButton = document.querySelector('#middle-button')
+      const boredomCount = document.querySelector('#boredom')
+      const boredomEnableButton = () => {
+        boredomButton.disabled = false
+      }
+
+      boredomButton.addEventListener('click', ()=> {
+        boredomButton.disabled = true;
+        boredomCount.style.padding = 5 + 'px';
+        character.boredomFunction()
+        boredomCount.innerText = `Boredom Level: ${character.boredom}`
+        setTimeout(boredomEnableButton, 5000)
+      })
+
+
+
+
+
+
+
+      //////////////// INCREASE SLEEPINESS LEVEL WITH TIME /////////////////
+      const intervalZ = setInterval(() => {
+        if (character.sleepiness <= 18) {
+          character.sleepiness += 2
+          tiredCount.innerText = `Tired Level: ${character.sleepiness}`
+
+        } else {
+          clearInterval(intervalZ)
+          clearInterval(intervalX)
+          clearInterval(intervalY)
+          clearInterval(ageInterval)
+          gameOver.style.opacity = 100
+          soldierCharacter.style.opacity = 0
+        }
+      }, 3000)
+
+
+      //////////////// DECREASE SLEEPINESS LEVEL WITH BUTTON (FEED) /////////////////
+        const sleepinessButton = document.querySelector('#right-button')
+        const tiredCount = document.querySelector('#sleepiness')
+        const sleepinessEnableButton = () => {
+          sleepinessButton.disabled = false
+        }
+
+        sleepinessButton.addEventListener('click', ()=> {
+          sleepinessButton.disabled = true;
+          tiredCount.style.padding = 5 + 'px';
+          character.sleepFunction()
+          tiredCount.innerText = `Tired Level: ${character.sleepiness}`
+          setTimeout(sleepinessEnableButton, 5000)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////// TOUCH SCREEN FUNCTION /////////////////
+
+// CAN I CLICK AND DRAG??? IF SO, HOW???
+
+// let screenLog = document.querySelector('#screen-log');
+// document.addEventListener('mousemove', logKey);
+//
+// function logKey(e) {
+//   screenLog.innerText = `
+//     Screen X/Y: ${e.screenX}, ${e.screenY}
+//     Client X/Y: ${e.clientX}, ${e.clientY}`;
+// // //
+// //
+// //
+// //
+// // // get the client cordinates for which to run the function in
+// // if clientX < rightCordinate || clientX > leftCordinate || clientY < bottomCordinate || clientY > topCordinate, run the function
+// if (e.clientX < 550 || e.clientX > 336 || e.clientY > 220 || e.clientY < 433 ) {
+//   if (e.clientX === 354 || e.clientY === 410) {
+//     const sleepButton = document.querySelector('#right-button')
+//     const sleepCount = document.querySelector('#sleepiness')
+//     sleepCount.innerText = `Tired Level: ${character.sleepiness}`
+//
+//     sleepButton.addEventListener('click', ()=> {
+//       sleepCount.style.padding = 5 + 'px'
+//       if (character.sleepiness > 1){
+//         character.sleepFunction()
+//         sleepCount.innerText = `Tired Level: ${character.sleepiness}`
+//       }
+//     })
+//   }
+// }
+// }
+
+
+// More (if Statements inside of function)
+// food: if clientX = ... || clientY = ... {run the same function as for food button}
+// sleep: if clientX = ... || clientY = ... {run the same function as for food button}
+// boredom: if clientX = ... || clientY = ... {run the same function as for food button}
+
+
+// if clientX = ... || client
+
+
+//////////////// INCREASE HUNGER LEVEL BY 30 SECS //////////////////
